b9583a999de8150350c31b807b75c420c5582859
nyvraxx <NyvraX@gmail.com>
2025-01-16 14:12:26 -0500

    Delete hex_world directory

--- hex_world/src/graphics/Drawable.java
+++
@@ -1,7 +0,0 @@
-package graphics;
-
-import java.awt.Graphics2D;
-
-public interface Drawable {
-	public abstract void draw(Graphics2D g2, RenderingContext renderingContext);
-}
--- hex_world/src/module-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * 
- */
-/**
- * 
- */
-module hex_world {
-	requires java.desktop;
-}
